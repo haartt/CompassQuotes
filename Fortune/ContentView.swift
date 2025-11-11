abefadb8dd25ddd56d2f8d7a5549365de942c5ee
haartt <fabioantonucci6@gmail.com>
2025-11-11 15:33:32 +0100

    Updated files

---
+++ Fortune/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fortune
+//
+//  Created by Fabio Antonucci on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fortune/FortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortuneApp.swift
+//  Fortune
+//
+//  Created by Fabio Antonucci on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
